6bc3cc679f220cc5620e90c40f9fb2d2b7a0566a
João Vinicius Rodrigues <joaovinicius2525@gmail.com>
2026-07-27 22:18:52 -0300

    Remove migrated static portfolio data

--- src/interfaces/experience.ts
+++
@@ -1,8 +0,0 @@
-export interface IExperience {
-  slug: string
-  company: string
-  startDate: string
-  finishDate?: string
-  skills: string[]
-  companyUrl?: string
-}
--- src/interfaces/project.ts
+++
@@ -1,9 +0,0 @@
-export interface IProject {
-  slug: string
-  title: string
-  technologies: string[]
-  repoUrl?: string
-  siteUrl?: string
-  status?: 'private' | 'public'
-  images?: string[]
-}
